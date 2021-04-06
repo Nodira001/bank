4059805e7573022de3c8d2fb9de2cabe0ef36f4b
Nodira001 <nodirapaizieva@yandex.ru>
2021-04-06 15:35:41 +0500

    implemented v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/NodiraPayzieva/bank
+module github.com/NodiraPayzieva/bank/v2
 
 go 1.16
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -8,5 +8,18 @@ type Payment	struct {
 	ID int
 	Amount Money
 	Category Category
+	Status Status
 
 }
+
+type Status string
+
+const (
+	StatusOk Status = "OK"
+	StatusFaiL Status = "Fail"
+	StatusInProgress Status ="Inprogress"
+	)
+
+
+
+
